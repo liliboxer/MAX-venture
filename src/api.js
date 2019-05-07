a5472cc4b589f3a069aea61c04bb0a634dc7de6e
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 12:19:29 -0700

    tdd for api

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const api = {
+    storage: localStorage,
+    signUp() {
+
+    },
+    getUser(user) {
+        return user;
+    }
+
+}
+
+export default api;
---
+++ test/apit-test.js
@@ -0,0 +1,31 @@
+import api from '../src/api.js';
+
+
+const test = QUnit.test;
+
+QUnit.model('api');
+api.storage = sessionStorage;
+
+QUnit.testStart(() => {
+    sessionStorage.clear();
+})
+
+
+test('signUp sets user and returns on getter', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const user = {
+        name: 'Lili',
+        cat: 'Max the napper',
+        fluff: 10,
+        hp: 50,
+        gold: 0
+    };
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = api.getUser();
+
+    //Assert
+    assert.deepEqual(result, user);
+});
