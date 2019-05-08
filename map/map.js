18c5079cf6bf5dde27c9cb434f4d9b0c1a2095ad
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 23:00:02 -0700

    updated file path for avatar.src

--- map/map.js
+++ map/map.js
@@ -1,7 +1,7 @@
 import api from '../src/api.js';
 
 const name = document.getElementById('name');
-const cat = document.getElementById('cat');
+const avatar = document.getElementById('avatar');
 const fluff = document.getElementById('fluff');
 const hp = document.getElementById('hp');
 const gold = document.getElementById('gold');
@@ -13,7 +13,7 @@ if(!user) {
 }
 
 name.textContent = user.name;
-cat.src = './assets/' + user.cat;
+avatar.src = '../assets/' + user.cat + '.png';
 fluff.textContent = 'fluff: ' + user.fluff;
 hp.textContent = 'hp: ' + user.hp;
 gold.textContent = 'gold: ' + user.gold;
