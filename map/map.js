cffc16980580259314fcb9eeb810a61a7674849f
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 22:47:56 -0700

    wiring profile pic to user profile

--- map/map.js
+++ map/map.js
@@ -1,7 +1,7 @@
 import api from '../src/api.js';
 
-const avatar = document.getElementById('avatar');
 const name = document.getElementById('name');
+const cat = document.getElementById('cat');
 const fluff = document.getElementById('fluff');
 const hp = document.getElementById('hp');
 const gold = document.getElementById('gold');
@@ -13,8 +13,7 @@ if(!user) {
 }
 
 name.textContent = user.name;
+cat.src = './assets/' + user.cat;
 fluff.textContent = 'fluff: ' + user.fluff;
 hp.textContent = 'hp: ' + user.hp;
 gold.textContent = 'gold: ' + user.gold;
-
-console.log(gold.textContent);
