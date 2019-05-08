d6582bae25727e5fd52b94d09c6269187e7103c7
Lili Boxer <ninaliboxer@gmail.com>
2019-05-07 21:58:05 -0700

    fluff range almost working

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import makeUser from './make-user.js';
 import api from './api.js';
 
 const userSignUp = document.getElementById('user-sign-up');
+const fluffRange = document.getElementById('fluff');
+const fluffDisplay = document.getElementById('fluff-display');
+
+fluffRange.addEventListener('change', () => {
+    fluffDisplay.textContent = fluffRange.value;
+});
+
 
 userSignUp.addEventListener('submit', () => {
     event.preventDefault();
