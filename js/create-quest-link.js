d23a23160822a50530e27be0e18ffbb7509a2187
Lili Boxer <ninaliboxer@gmail.com>
2019-05-09 11:22:30 -0700

    tdd cleaned up

--- js/create-quest-link.js
+++ js/create-quest-link.js
@@ -1,13 +1,10 @@
 function createQuestLink(quest) {
     const link = document.createElement('a');
     const h3 = document.createElement('h3');
-    const appendedH3 = h3.appendChild(link);
-    console.log(h3)
-    console.log(appendedH3);
+    h3.appendChild(link);
 
     const searchParams = new URLSearchParams();
     searchParams.set('id', quest.id);
-
 
     link.href = 'quest.html?' + searchParams.toString();
 
