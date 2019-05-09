78127a3b93a0468039c97f3b5db57407b6d2ecc1
Lili Boxer <ninaliboxer@gmail.com>
2019-05-09 12:17:17 -0700

    repopulated map window with JS

--- js/map.js
+++ js/map.js
@@ -1,5 +1,16 @@
 import api from './api.js';
 import loadProfile from './load-profile.js';
+import createQuestLink from './create-quest-link.js';
 
 loadProfile();
 
+const quests = api.getQuests();
+
+const nav = document.getElementById('quest-options');
+
+for (let i = 0; i < quests.length; i++) {
+    const quest = quests[i];
+    const link = createQuestLink(quest);
+    nav.appendChild(link);
+    
+}
