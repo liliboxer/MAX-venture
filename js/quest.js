cea33acf537583da560dff8ee25acec6217fdc56
Lili Boxer <ninaliboxer@gmail.com>
2019-05-10 11:16:23 -0700

    quest page almost fully loaded

--- js/quest.js
+++ js/quest.js
@@ -2,5 +2,20 @@ import api from './api.js';
 import createChoice from './create-choice.js';
 import loadProfile from './load-profile.js';
 
-const user = api.getUser();
 loadProfile();
+
+const searchParams = new URLSearchParams(window.location.search);
+const questId = searchParams.get('id');
+
+const quest = api.getQuest(questId);
+const user = api.getUser();
+
+const title = document.getElementById('title');
+const image = document.getElementById('image');
+const description = document.getElementById('description');
+const choiceForm = document.getElementById('choice-form');
+const result = document.getElementById('result');
+
+title.textContent = quest.title;
+image.src = './assets/' + quest.id + '.jpg';
+description.textContent = quest.description;
